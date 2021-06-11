6e89f6088c476ecdcd34bd97d3e8e9f533422956
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-11 19:18:41 +0000

    Create initial `ezrpc::tower` macro entry point
    The plan is to generate `Request`, `Response` and `Service` types from
    an `impl` item.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn tower(_attribute: TokenStream, item_tokens: TokenStream) -> TokenStream {
+    item_tokens
 }

b786035d75b69f550671843588de06ac30277d73
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-11 20:12:42 +0000

    Parse item tokens
    Transform the `TokenStream` into a `sync::ItemImpl` so that the
    necessary information can be extracted later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-use proc_macro::TokenStream;
+use {
+    proc_macro::TokenStream,
+    quote::quote,
+    syn::{parse_macro_input, ItemImpl},
+};
 
 #[proc_macro_attribute]
 pub fn tower(_attribute: TokenStream, item_tokens: TokenStream) -> TokenStream {
-    item_tokens
+    let item = parse_macro_input!(item_tokens as ItemImpl);
+
+    TokenStream::from(quote! { #item })
 }

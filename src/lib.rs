4b3080e3c1865899e78b31ac6bf8e1830d07fd44
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-27 20:17:50 +0000

    Move `Service` type generation into `Generator`
    Refactor to move the final code generation piece into `Generator`.
    Create a method to generate the `Service` type and its `tower::Service`
    implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 mod tower;
 
 use {
-    crate::tower::{Generator, MethodData},
+    crate::tower::Generator,
     proc_macro::TokenStream as RawTokenStream,
-    proc_macro2::TokenStream,
     quote::quote,
     syn::{parse_macro_input, ItemImpl},
 };
@@ -13,66 +12,11 @@ pub fn tower(_attribute: RawTokenStream, item_tokens: RawTokenStream) -> RawToke
     let item = parse_macro_input!(item_tokens as ItemImpl);
     let generator = Generator::new(&item);
     let request = generator.request();
-    let service = build_service(&generator);
+    let service = generator.service();
 
     RawTokenStream::from(quote! {
         #request
         #item
         #service
     })
-}
-
-fn build_service(generator: &Generator) -> TokenStream {
-    let service_impl = build_service_impl(generator);
-    let service_methods = build_service_methods(generator);
-
-    quote! {
-        pub struct Service;
-
-        #service_impl
-        #service_methods
-    }
-}
-
-fn build_service_impl(generator: &Generator) -> TokenStream {
-    let result_data = generator.result();
-    let response = result_data.ok_type();
-    let error = result_data.err_type();
-    let request_calls = generator
-        .methods()
-        .iter()
-        .map(|method| method.request_match_arm(generator.self_type()));
-
-    quote! {
-        impl tower::Service<Request> for Service {
-            type Response = #response;
-            type Error = #error;
-            type Future = std::pin::Pin<Box<
-                dyn std::future::Future<Output = Result<Self::Response, Self::Error>>
-            >>;
-
-            fn poll_ready(
-                &mut self,
-                context: &mut std::task::Context<'_>,
-            ) -> std::task::Poll<Result<(), Self::Error>> {
-                std::task::Poll::Ready(Ok(()))
-            }
-
-            fn call(&mut self, request: Request) -> Self::Future {
-                match request {
-                    #( #request_calls ),*
-                }
-            }
-        }
-    }
-}
-
-fn build_service_methods(generator: &Generator) -> TokenStream {
-    let service_methods = generator.methods().iter().map(MethodData::service_method);
-
-    quote! {
-        impl Service {
-            #( #service_methods )*
-        }
-    }
 }
--- src/tower/generator.rs
+++ src/tower/generator.rs
@@ -67,4 +67,59 @@ impl Generator {
             }
         }
     }
+
+    /// Generate the `Service` type and its [`tower::Service`] implementation.
+    ///
+    /// The `Service` type receives `Request`s and dispatches them to the method implementations in
+    /// the input `impl` block.
+    pub fn service(&self) -> TokenStream {
+        let service_impl = self.service_impl();
+        let service_methods = self.methods.iter().map(MethodData::service_method);
+
+        quote! {
+            pub struct Service;
+
+            impl Service {
+                #( #service_methods )*
+            }
+
+            #service_impl
+        }
+    }
+
+    /// Generate the implementation of the [`tower::Service`] trait for the generated `Service`
+    /// type.
+    ///
+    /// The implementation is a large dispatcher, that calls the methods in the input `impl` block.
+    fn service_impl(&self) -> TokenStream {
+        let request_match_arms = self
+            .methods
+            .iter()
+            .map(|method| method.request_match_arm(&self.self_type));
+        let response = self.result.ok_type();
+        let error = self.result.err_type();
+
+        quote! {
+            impl tower::Service<Request> for Service {
+                type Response = #response;
+                type Error = #error;
+                type Future = std::pin::Pin<Box<
+                    dyn std::future::Future<Output = Result<Self::Response, Self::Error>>
+                >>;
+
+                fn poll_ready(
+                    &mut self,
+                    context: &mut std::task::Context<'_>,
+                ) -> std::task::Poll<Result<(), Self::Error>> {
+                    std::task::Poll::Ready(Ok(()))
+                }
+
+                fn call(&mut self, request: Request) -> Self::Future {
+                    match request {
+                        #( #request_match_arms ),*
+                    }
+                }
+            }
+        }
+    }
 }

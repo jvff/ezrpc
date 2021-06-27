3a0b8207e53c753151f2bcfcb6b50c4db7ccb3c0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-27 20:23:59 +0000

    Remove `RawTokenStream` type alias
    The usage of different `TokenStream` types is now properly isolated, so
    creating a `RawTokenStream` type to differentiate it from the
    `proc_macro2::TokenStream` type is no longer necessary.

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@ mod tower;
 
 use {
     crate::tower::Generator,
-    proc_macro::TokenStream as RawTokenStream,
+    proc_macro::TokenStream,
     quote::quote,
     syn::{parse_macro_input, ItemImpl},
 };
 
 #[proc_macro_attribute]
-pub fn tower(_attribute: RawTokenStream, item_tokens: RawTokenStream) -> RawTokenStream {
+pub fn tower(_attribute: TokenStream, item_tokens: TokenStream) -> TokenStream {
     let item = parse_macro_input!(item_tokens as ItemImpl);
     let generator = Generator::new(&item);
     let request = generator.request();
     let service = generator.service();
 
-    RawTokenStream::from(quote! {
+    TokenStream::from(quote! {
         #request
         #item
         #service

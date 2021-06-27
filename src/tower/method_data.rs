d01a5bc9529b545833eb77b3af891a61022e493b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-27 04:42:28 +0000

    Generate `Request` variant names in `MethodData`
    Generate it once in one place, so that other code can just reuse it.

--- src/tower/method_data.rs
+++ src/tower/method_data.rs
@@ -1,5 +1,6 @@
 use {
     super::{parameter_data::ParameterData, result_data::ResultData},
+    heck::CamelCase,
     syn::{FnArg, Ident, ImplItemMethod},
 };
 
@@ -7,6 +8,11 @@ use {
 pub struct MethodData {
     /// The name of the method.
     name: Ident,
+
+    /// The name of the generated `Request` variant.
+    ///
+    /// This is equivalent to the name of the method converted to CamelCase.
+    request_name: Ident,
 
     /// The parameters of the method.
     ///
@@ -21,6 +27,8 @@ impl MethodData {
     /// Create a new [`MethodData`] by parsing an [`ImplItemMethod`] syntax tree.
     pub fn new(method: &ImplItemMethod) -> Self {
         let name = method.sig.ident.clone();
+        let request_name_string = name.to_string().to_camel_case();
+        let request_name = Ident::new(&request_name_string, name.span());
 
         let parameters = method
             .sig
@@ -37,6 +45,7 @@ impl MethodData {
 
         MethodData {
             name,
+            request_name,
             parameters,
             result,
         }
@@ -45,6 +54,13 @@ impl MethodData {
     /// Retrieve the name of this method.
     pub fn name(&self) -> &Ident {
         &self.name
+    }
+
+    /// Retrieve the name of the generated `Request` variant respective to this method.
+    ///
+    /// This is equivalent to the name of the method converted to CamelCase.
+    pub fn request_name(&self) -> &Ident {
+        &self.request_name
     }
 
     /// Retrieve the list of [`ParameterData`] for this method's parameters.

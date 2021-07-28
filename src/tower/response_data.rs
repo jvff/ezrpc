fb03e301eaa87a0725b5544a9de357b3a4d1c5b3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-07-28 18:20:01 +0000

    Better determine response `Result` type
    Go through all the methods to find the best response type. Previously,
    only the first method determined the response type, but that could be a
    method that doesn't return an error. Now, all method return types are
    reduced into a single response type. For now this type should be a
    common `Result` type, and that assumes that all methods have either the
    common `Result` type as the return type, or just the result's wrapped
    `Ok` type.

--- src/tower/response_data.rs
+++ src/tower/response_data.rs
@@ -1,6 +1,7 @@
 use {
     super::{method_data::MethodData, result_data::ResultData},
     proc_macro2::TokenStream,
+    proc_macro_error::abort,
 };
 
 /// Representation of the RPC response type.
@@ -12,7 +13,12 @@ pub struct ResponseData {
 impl ResponseData {
     /// Create a new [`ResponseData`] from the list of RPC methods.
     pub fn new(methods: &[MethodData]) -> Self {
-        let result = methods[0].result().clone();
+        let result = methods
+            .iter()
+            .map(MethodData::result)
+            .reduce(Self::merge_method_results)
+            .cloned()
+            .expect("Empty slice of methods in `ResponseData::new`");
 
         ResponseData { result }
     }
@@ -25,5 +31,26 @@ impl ResponseData {
     /// Return the [`Err`][Result::Err] type that's expected from the RPC call.
     pub fn err_type(&self) -> TokenStream {
         self.result.err_type()
+    }
+
+    /// Merge the [`ResultData`] from two methods.
+    fn merge_method_results<'r>(
+        first_result: &'r ResultData,
+        second_result: &'r ResultData,
+    ) -> &'r ResultData {
+        match (&first_result, second_result) {
+            (ResultData::Result { ok_type, .. }, ResultData::NotResult(second))
+                if ok_type == second =>
+            {
+                first_result
+            }
+            (ResultData::NotResult(first), ResultData::Result { ok_type, .. })
+                if ok_type == first =>
+            {
+                second_result
+            }
+            (first, second) if *first == second => first_result,
+            _ => abort!(second_result, "Incompatible method return type"),
+        }
     }
 }

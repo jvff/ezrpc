6e1c7e5a0089c26c3a4a4ae7ae00f7426640135a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-13 23:29:01 +0000

    Add `ReceiverType::service_method_call_prefix`
    A method to help with handling the different ways to call a method,
    depending on the service receiver type used to determine the service
    data and the method receiver type. This generates code that can then be
    concatenated with the method name and the method paramaters in order to
    call the method.

--- src/tower/receiver_type.rs
+++ src/tower/receiver_type.rs
@@ -1,4 +1,9 @@
-use {proc_macro_error::abort, syn::FnArg};
+use {
+    proc_macro2::TokenStream,
+    proc_macro_error::abort,
+    quote::quote,
+    syn::{FnArg, Type},
+};
 
 /// The receiver type of the method.
 ///
@@ -37,6 +42,49 @@ impl ReceiverType {
             }
         } else {
             ReceiverType::NoReceiver
+        }
+    }
+
+    /// Generate the code necessary for calling a method from the generated `Service` type.
+    ///
+    /// This requires to [`ReceiverType`]s. One that is more strict, for the common receiver type
+    /// for the service, and one that can be more relaxed, for the method to be called. It is
+    /// assumed that this method will be called on the more strict receiver type and it will
+    /// receive the `method_receiver_type` as an extra parameter.
+    pub fn service_method_call_prefix(
+        &self,
+        method_receiver_type: ReceiverType,
+        self_type: &Type,
+    ) -> TokenStream {
+        match (self, method_receiver_type) {
+            (
+                ReceiverType::NoReceiver | ReceiverType::Reference | ReceiverType::MutableReference,
+                ReceiverType::NoReceiver,
+            ) => {
+                quote! { #self_type:: }
+            }
+
+            (ReceiverType::Reference, ReceiverType::Reference) => quote! {
+                inner.
+            },
+
+            (ReceiverType::MutableReference, ReceiverType::Reference) => quote! {
+                inner.read().await.
+            },
+
+            (ReceiverType::MutableReference, ReceiverType::MutableReference) => quote! {
+                inner.write().await.
+            },
+
+            (
+                ReceiverType::NoReceiver,
+                ReceiverType::Reference | ReceiverType::MutableReference,
+            )
+            | (ReceiverType::Reference, ReceiverType::MutableReference) => {
+                unreachable!(
+                    "Service receiver type should always be stricter than method receiver type"
+                )
+            }
         }
     }
 }

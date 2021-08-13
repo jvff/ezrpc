0be0bf163b14a4e8459ce89662b8ab90aead76d9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-13 01:06:16 +0000

    Update example to restore method order
    The response type is no longer determined by the first method's return
    type.

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -2,16 +2,16 @@ pub struct Example;
 
 #[ezrpc::tower]
 impl Example {
+    pub async fn echo(string: String) -> String {
+        string
+    }
+
     pub async fn reverse(string: String) -> Result<String, EmptyString> {
         if !string.is_empty() {
             Ok(string.chars().rev().collect())
         } else {
             Err(EmptyString)
         }
-    }
-
-    pub async fn echo(string: String) -> String {
-        string
     }
 }
 
--- example/src/lib_after_macro_expansion.rs
+++ example/src/lib_after_macro_expansion.rs
@@ -1,11 +1,21 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
 pub struct Example;
 
 pub enum Request {
-    Reverse { string: String },
     Echo { string: String },
+    Reverse { string: String },
 }
 
 impl Example {
+    pub async fn echo(string: String) -> String {
+        string
+    }
+
     pub async fn reverse(string: String) -> Result<String, EmptyString> {
         if !string.is_empty() {
             Ok(string.chars().rev().collect())
@@ -13,26 +23,11 @@ impl Example {
             Err(EmptyString)
         }
     }
-
-    pub async fn echo(string: String) -> String {
-        string
-    }
 }
 
 pub struct Service;
 
 impl Service {
-    pub async fn reverse(&mut self, string: String) -> ::std::result::Result<String, EmptyString> {
-        use tower::{Service as _, ServiceExt as _};
-
-        let service = self
-            .ready()
-            .await
-            .expect("Generated service is always ready");
-
-        service.call(Request::Reverse { string }).await
-    }
-
     pub async fn echo(&mut self, string: String) -> String {
         use tower::{Service as _, ServiceExt as _};
 
@@ -46,27 +41,34 @@ impl Service {
             .await
             .expect("Result data never fails")
     }
+
+    pub async fn reverse(&mut self, string: String) -> Result<String, EmptyString> {
+        use tower::{Service as _, ServiceExt as _};
+
+        let service = self
+            .ready()
+            .await
+            .expect("Generated service is always ready");
+
+        service.call(Request::Reverse { string }).await
+    }
 }
 
 impl tower::Service<Request> for Service {
     type Response = String;
     type Error = EmptyString;
-    type Future =
-        std::pin::Pin<Box<dyn std::future::Future<Output = Result<Self::Response, Self::Error>>>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
 
-    fn poll_ready(
-        &mut self,
-        context: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), Self::Error>> {
-        std::task::Poll::Ready(Ok(()))
+    fn poll_ready(&mut self, context: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, request: Request) -> Self::Future {
         use futures::FutureExt as _;
 
         match request {
-            Request::Reverse { string } => futures::FutureExt::boxed(Example::reverse(string)),
             Request::Echo { string } => futures::FutureExt::boxed(Example::echo(string).map(Ok)),
+            Request::Reverse { string } => futures::FutureExt::boxed(Example::reverse(string)),
         }
     }
 }
